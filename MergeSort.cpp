0f6b9f68aa971a864e840fff7a1fad41e514b842
fathannnur <carelius02@gmail.com>
2026-04-02 10:59:20 +0700

    Menambahkan int main

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -94,7 +94,14 @@ void output()
     cout << "\nData setelah diurutkan (Merge Sort): ";
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << " "; step 4.f.i
+        cout << arr[i] << " ";
     }
     cout << endl;
+}
+
+int main()
+{
+    input();
+    mergeSort(0, n - 1);
+    output();
 }
